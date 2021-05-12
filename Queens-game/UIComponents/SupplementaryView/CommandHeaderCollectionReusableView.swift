7625ec12fcc43bc7b064705e1a9e3ffb357cd91d
cookie777 <l.oeil.ballon@gmail.com>
2021-05-11 18:30:01 -0700

    feat: update view

--- Queens-game/UIComponents/SupplementaryView/CommandHeaderCollectionReusableView.swift
+++ Queens-game/UIComponents/SupplementaryView/CommandHeaderCollectionReusableView.swift
@@ -8,5 +8,28 @@
 import UIKit
 
 class CommandHeaderCollectionReusableView: UICollectionReusableView {
-        
+  static let identifier = "command header"
+  
+  let title = H2Label(text: "Edit commands")
+  let subTitle = H3Label(text: "Command list")
+  
+  lazy var stackView: VerticalStackView = {
+    let uv = UIView()
+    let sv = VerticalStackView(
+      arrangedSubviews: [uv, title, subTitle]
+    )
+    sv.setCustomSpacing(64, after: uv)
+    sv.setCustomSpacing(24, after: title)
+    return sv
+  } ()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    stackView.configSuperView(under: self)
+    stackView.matchParent()
+  }
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
 }
