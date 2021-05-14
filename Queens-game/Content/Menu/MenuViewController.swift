5fd6c1c81828f57ba485bbf47fb78bf9346c5b0d
takayasu.nasu <taka.beckham@gmail.com>
2021-05-13 19:11:23 -0700

    Added StackView at MenuVC.

--- Queens-game/Content/Menu/MenuViewController.swift
+++ Queens-game/Content/Menu/MenuViewController.swift
@@ -9,32 +9,24 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupLayout()
-  }
-  
-  let screenName: UILabel = {
-    let lb = UILabel()
+  let screenTitle: H2Label = {
+    let lb = H2Label(text: "Menu")
     lb.translatesAutoresizingMaskIntoConstraints = false
-    lb.text = "Menu"
-    
+    lb.lineBreakMode = .byWordWrapping
+    lb.numberOfLines = 0
+    lb.setContentHuggingPriority(.required, for: .vertical)
     return lb
   }()
-  
+
   let closeButton: UIButton = {
     let bt = UIButton()
     bt.translatesAutoresizingMaskIntoConstraints = false
     bt.setTitle("Close", for: .normal)
     bt.backgroundColor = .black
     bt.setTitleColor(.white, for: .normal)
-    bt.addTarget(self, action: #selector(closeTapped(_:)), for: .touchUpInside)
     return bt
   }()
 
-  @objc func closeTapped(_ sender: UIButton) {
-    dismiss(animated: true, completion: nil)
-  }
   
   let howToPlayButton: UIButton = {
     let bt = UIButton()
@@ -48,7 +40,7 @@ class MenuViewController: UIViewController {
 
   @objc func howToPlaytapped(_ sender: UIButton) {
     let nx = RuleBookViewController()
-  present(nx, animated: true, completion: nil)
+    present(nx, animated: true, completion: nil)
   }
   
   let settingButton: UIButton = {
@@ -74,34 +66,62 @@ class MenuViewController: UIViewController {
     return bt
   }()
 
+  lazy var stackView: VerticalStackView = {
+    let sv = VerticalStackView(
+      arrangedSubviews: [
+        screenTitle,
+        closeButton,
+        howToPlayButton,
+        settingButton,
+        goToTopButton
+      ]
+    )
+    sv.alignment = .fill
+    sv.distribution = .equalSpacing
+    sv.translatesAutoresizingMaskIntoConstraints = false
+    return sv
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupLayout()
+  }
+
+}
+
+extension MenuViewController {
+
+  private func setupLayout() {
+    view.configBgColor(bgColor: CustomColor.background)
+    navigationItem.hidesBackButton = true
+    navigationController?.navigationBar.barTintColor = CustomColor.background
+    navigationController?.navigationBar.shadowImage = UIImage()
+    navigationItem.setRightBarButton(
+      UIBarButtonItem(customView: closeButton),
+      animated: true
+    )
+    view.addSubview(stackView)
+    stackView.topAnchor.constraint(
+      equalTo: view.topAnchor,
+      constant: Constant.Common.topSpacing
+    ).isActive = true
+    stackView.bottomAnchor.constraint(
+      equalTo: view.bottomAnchor,
+      constant: Constant.Common.bottomSpacing
+    ).isActive = true
+    stackView.leadingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+      constant: Constant.Common.leadingSpacing
+    ).isActive = true
+    stackView.trailingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+      constant:  Constant.Common.trailingSpacing
+    ).isActive = true
+
+  }
+
   @objc func goToTop(_ sender: UIButton) {
     let nx = ExitAlertViewController()
     present(nx, animated: true, completion: nil)
-  }
-
-  private func setupLayout() {
-    view.backgroundColor = .white
-    navigationItem.hidesBackButton = true
-
-    view.addSubview(screenName)
-    view.addSubview(closeButton)
-    view.addSubview(howToPlayButton)
-    view.addSubview(settingButton)
-    view.addSubview(goToTopButton)
-
-    screenName.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    screenName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-
-    closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    closeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    
-    howToPlayButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    howToPlayButton.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: 10).isActive = true
-
-    settingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    settingButton.topAnchor.constraint(equalTo: howToPlayButton.bottomAnchor, constant: 10).isActive = true
-
-    goToTopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    goToTopButton.topAnchor.constraint(equalTo: settingButton.bottomAnchor, constant: 10).isActive = true
   }
 }
