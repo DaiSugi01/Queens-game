d8946f85effe90491be08d62ce7502dacae1cf3c
takayasu.nasu <taka.beckham@gmail.com>
2021-05-14 17:47:22 -0700

    Modified Citizen Selected and Result VC.

--- Queens-game/Content/CitizenSelected/CitizenSelectedViewController.swift
+++ Queens-game/Content/CitizenSelected/CitizenSelectedViewController.swift
@@ -6,67 +6,177 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class CitizenSelectedViewController: UIViewController {
+
+  let viewModel = CitizenSelectedViewModel()
+
+  let disposeBag = DisposeBag()
+
+  let screenTitle: H2Label = {
+    let lb = H2Label(text: "Selecting the citizen...")
+    lb.translatesAutoresizingMaskIntoConstraints = false
+    lb.lineBreakMode = .byWordWrapping
+    lb.numberOfLines = 0
+    lb.setContentHuggingPriority(.required, for: .vertical)
+    return lb
+  }()
+
+  let skipButton: SubButton = {
+    let button = SubButton()
+    button.setTitle("Skip", for: .normal)
+    button.addTarget(self, action: #selector(skipButtonTapped(_:)), for: .touchUpInside)
+    return button
+  }()
+
+  let suits: [UILabel] = {
+    let suits = ["♠","♦","♥","♣"]
+    let suitLabels: [UILabel] = suits.map { suit in
+      let label = UILabel()
+      label.text = suit
+      label.font = CustomFont.h2
+      if ["♦","♥"].contains(suit) {
+        label.textColor = .red
+      }
+      return label
+    }
+    return suitLabels
+  }()
+
+  lazy var above: HorizontalStackView = {
+    let stackView = HorizontalStackView(
+      arrangedSubviews: [suits[0], suits[1]],
+      distribution: .equalSpacing
+    )
+    return stackView
+  }()
+
+  lazy var countdownLabel: UILabel = {
+    let label = UILabel()
+    label.text = String(self.viewModel.countdonwTime)
+    label.font = CustomFont.h1
+    label.textAlignment = .center
+    label.sizeToFit()
+    return label
+  }()
+
+  lazy var sec: UILabel = {
+    let label = UILabel()
+    label.text = "sec"
+    label.textAlignment = .center
+    return label
+  }()
+
+  lazy var center: VerticalStackView = {
+    let stackView = VerticalStackView(
+      arrangedSubviews: [countdownLabel, sec],
+      distribution: .equalSpacing
+    )
+    return stackView
+  }()
+
+  lazy var below: HorizontalStackView = {
+    let stackView = HorizontalStackView(
+      arrangedSubviews: [suits[2], suits[3]],
+      distribution: .equalSpacing
+    )
+    return stackView
+  }()
+
+  lazy var countdownBlock: VerticalStackView = {
+    let stackView = VerticalStackView(
+      arrangedSubviews: [
+        self.above,
+        self.center,
+        self.below
+      ]
+    )
+    stackView.spacing = 32
+    return stackView
+  }()
+
+  lazy var wrapperCountdownBlock: VerticalStackView = {
+    let stackView = VerticalStackView(arrangedSubviews: [countdownBlock])
+    stackView.alignment = .center
+    return stackView
+  }()
+
+  lazy var stackView: VerticalStackView = {
+    let sv = VerticalStackView(
+      arrangedSubviews: [
+        screenTitle,
+        wrapperCountdownBlock,
+        skipButton
+      ]
+    )
+    sv.alignment = .fill
+    sv.distribution = .equalSpacing
+    return sv
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setupLayout()
+    //    self.viewModel.countdown()
+    self.viewModel.rxCountdownTime
+      .subscribe(onNext: { [weak self] time in
+        self?.countdownLabel.text = String(time!)
+      },
+      onCompleted: {
+        self.replaceView()
+      })
+      .disposed(by: disposeBag)
   }
   
-  let screenName: UILabel = {
-    let lb = UILabel()
-    lb.translatesAutoresizingMaskIntoConstraints = false
-    lb.text = "Citizen Selected"
-    
-    return lb
-  }()
-  
-  let nextButton: UIButton = {
-    let bt = UIButton()
-    bt.translatesAutoresizingMaskIntoConstraints = false
-    bt.setTitle("Next", for: .normal)
-    bt.backgroundColor = .black
-    bt.setTitleColor(.white, for: .normal)
-    bt.addTarget(self, action: #selector(goToNext(_:)), for: .touchUpInside)
-    
-    return bt
-  }()
-  
-  let backButton: UIButton = {
-    let bt = UIButton()
-    bt.translatesAutoresizingMaskIntoConstraints = false
-    bt.setTitle("Back", for: .normal)
-    bt.setTitleColor(.black, for: .normal)
-    bt.addTarget(self, action: #selector(goBack(_:)), for: .touchUpInside)
-    
-    return bt
-  }()
-  
-  @objc func goToNext(_ sender: UIButton) {
-    let nx = ResultViewController()
-    navigationController?.pushViewController(nx, animated: true)
-  }
-  
-  @objc func goBack(_ sender: UIButton) {
-    navigationController?.popViewController(animated: true)
-  }
-  
+}
+
+extension CitizenSelectedViewController {
+
   private func setupLayout() {
-    view.backgroundColor = .white
+    view.configBgColor(bgColor: CustomColor.background)
     navigationItem.hidesBackButton = true
 
-    view.addSubview(screenName)
-    view.addSubview(nextButton)
-    view.addSubview(backButton)
-    
-    screenName.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    screenName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-    
-    nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    
-    backButton.trailingAnchor.constraint(equalTo: nextButton.leadingAnchor, constant: -10).isActive = true
-    backButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    view.addSubview(stackView)
+    stackView.topAnchor.constraint(
+      equalTo: view.topAnchor,
+      constant: Constant.Common.topSpacing
+    ).isActive = true
+    stackView.bottomAnchor.constraint(
+      equalTo: view.bottomAnchor,
+      constant: Constant.Common.bottomSpacing
+    ).isActive = true
+    stackView.leadingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+      constant: Constant.Common.leadingSpacing
+    ).isActive = true
+    stackView.trailingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+      constant:  Constant.Common.trailingSpacing
+    ).isActive = true
+    countdownBlock.widthAnchor.constraint(
+      equalTo: wrapperCountdownBlock.widthAnchor,
+      multiplier: 0.8
+    ).isActive = true
   }
+
+  @objc func skipButtonTapped(_ sender: UIButton) {
+    self.stackView.removeFromSuperview()
+    self.toResult()
+  }
+
+  private func toResult() {
+    let nx = ResultViewController()
+    GameManager.shared.pushGameProgress(
+      navVC: navigationController!,
+      currentScreen: self,
+      nextScreen: nx
+    )
+  }
+
+  private func replaceView() {
+    self.stackView.removeFromSuperview()
+  }
+
 }
--- Queens-game/Content/Result/ResultViewController.swift
+++ Queens-game/Content/Result/ResultViewController.swift
@@ -9,64 +9,93 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupLayout()
-  }
-  
-  let screenName: UILabel = {
-    let lb = UILabel()
+  let navButtons = NextAndBackButtons()
+
+  let screenTitle: H2Label = {
+    let lb = H2Label(text: "It’s time to carry out ")
     lb.translatesAutoresizingMaskIntoConstraints = false
-    lb.text = "Result"
-    
+    lb.lineBreakMode = .byWordWrapping
+    lb.numberOfLines = 0
+    lb.setContentHuggingPriority(.required, for: .vertical)
     return lb
   }()
-  
-  let playAgainButton: UIButton = {
-    let bt = UIButton()
-    bt.translatesAutoresizingMaskIntoConstraints = false
-    bt.setTitle("Play again", for: .normal)
-    bt.backgroundColor = .black
-    bt.setTitleColor(.white, for: .normal)
-    bt.addTarget(self, action: #selector(goToNext(_:)), for: .touchUpInside)
-    
-    return bt
+
+  lazy var stackView: VerticalStackView = {
+    let sv = VerticalStackView(
+      arrangedSubviews: [
+        screenTitle,
+        navButtons,
+      ]
+    )
+    sv.alignment = .fill
+    sv.distribution = .equalSpacing
+    return sv
   }()
-  
-  let backToTopButton: UIButton = {
-    let bt = UIButton()
-    bt.translatesAutoresizingMaskIntoConstraints = false
-    bt.setTitle("Back to start menu", for: .normal)
-    bt.setTitleColor(.black, for: .normal)
-    bt.addTarget(self, action: #selector(goBack(_:)), for: .touchUpInside)
-    
-    return bt
-  }()
-  
-  @objc func goToNext(_ sender: UIButton) {
-    let previous = navigationController!.viewControllers[1]
-    navigationController?.popToViewController(previous, animated: true)
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.setupLayout()
+    self.configureButtons()
   }
-  
-  @objc func goBack(_ sender: UIButton) {
-    navigationController?.popToRootViewController(animated: true)
-  }
-  
+}
+
+extension ResultViewController {
+
   private func setupLayout() {
-    view.backgroundColor = .white
+    view.configBgColor(bgColor: CustomColor.background)
     navigationItem.hidesBackButton = true
 
-    view.addSubview(screenName)
-    view.addSubview(playAgainButton)
-    view.addSubview(backToTopButton)
-    
-    screenName.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    screenName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-    
-    playAgainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    playAgainButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    
-    backToTopButton.trailingAnchor.constraint(equalTo: playAgainButton.leadingAnchor, constant: -10).isActive = true
-    backToTopButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    view.addSubview(stackView)
+    stackView.topAnchor.constraint(
+      equalTo: view.topAnchor,
+      constant: Constant.Common.topSpacing
+    ).isActive = true
+    stackView.bottomAnchor.constraint(
+      equalTo: view.bottomAnchor,
+      constant: Constant.Common.bottomSpacing
+    ).isActive = true
+    stackView.leadingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+      constant: Constant.Common.leadingSpacing
+    ).isActive = true
+    stackView.trailingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+      constant:  Constant.Common.trailingSpacing
+    ).isActive = true
+
   }
+
+  private func configureButtons() {
+    navButtons.nextButton.setTitle("Play again", for: .normal)
+    navButtons.nextButton.titleLabel?.font = CustomFont.h4
+    navButtons.nextButton.addTarget(
+      self,
+      action: #selector(toStart),
+      for: .touchUpInside
+    )
+    
+    navButtons.backButton.setTitle("Back to start menu", for: .normal)
+    navButtons.backButton.titleLabel?.font = CustomFont.h4
+    navButtons.backButton.titleLabel?.lineBreakMode = .byWordWrapping
+    navButtons.backButton.addTarget(
+      self,
+      action: #selector(toTop),
+      for: .touchUpInside
+    )
+
+  }
+
+  @objc private func toStart() {
+    let nx = PlayerSelectionViewController()
+    GameManager.shared.pushGameProgress(
+      navVC: navigationController!,
+      currentScreen: self,
+      nextScreen: nx
+    )
+  }
+
+  @objc private func toTop() {
+    super.navigationController?.popToRootViewController(animated: true)
+  }
+  
 }
